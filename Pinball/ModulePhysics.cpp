b93f20af75149bdecaab43484869b5722cf57290
Lladruc <59049906+Lladruc37@users.noreply.github.com>
2020-11-02 20:21:45 +0100

    Changed lib folder names

--- Pinball/ModulePhysics.cpp
+++ Pinball/ModulePhysics.cpp
@@ -7,9 +7,9 @@
 #include "math.h"
 
 #ifdef _DEBUG
-#pragma comment( lib, "Box2D/libx86/Debug/Box2D.lib" )
+#pragma comment( lib, "Box2D/libx86/DebugLib/Box2D.lib" )
 #else
-#pragma comment( lib, "Box2D/libx86/Release/Box2D.lib" )
+#pragma comment( lib, "Box2D/libx86/ReleaseLib/Box2D.lib" )
 #endif
 
 ModulePhysics::ModulePhysics(Application* app, bool start_enabled) : Module(app, start_enabled)
